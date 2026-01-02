723341dadbe3c449ba58d35af5cde4acd8d6ffc7
Geralja <cocaiatem@gmail.com>
2026-01-01 22:49:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,6 @@ CATEGORIAS_OFICIAIS = [
     "Veterinário(a)",
     "Web Designer"
 ]
-    
     # --- MANUTENÇÃO E REFORMAS ---
     "Encanador", "Eletricista", "Pintor", "Pedreiro", "Gesseiro", "Telhadista", 
     "Serralheiro", "Vidraceiro", "Marceneiro", "Marmoraria", "Calhas e Rufos", 
@@ -911,6 +910,7 @@ except:
     ano_atual = 2025 # Valor padrão caso o módulo falhe
 
 st.markdown(f'<div style="text-align:center; padding:20px; color:#94A3B8; font-size:10px;">GERALJÁ v20.0 © {ano_atual}</div>', unsafe_allow_html=True)
+
 
 
 
