aaa930c8939542cf34f359a9c28947751417a885
Geralja <cocaiatem@gmail.com>
2026-01-01 22:19:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,6 @@ BONUS_WELCOME = 5
 LAT_REF = -23.5505
 LON_REF = -46.6333
 
-# --- LISTA DE CATEGORIAS COMPLETA E ALFABÉTICA ---
 CATEGORIAS_OFICIAIS = [
     "Acompanhante de Idosos",
     "Adestrador de Cães",
@@ -911,6 +910,7 @@ except:
     ano_atual = 2025 # Valor padrão caso o módulo falhe
 
 st.markdown(f'<div style="text-align:center; padding:20px; color:#94A3B8; font-size:10px;">GERALJÁ v20.0 © {ano_atual}</div>', unsafe_allow_html=True)
+
 
 
 
